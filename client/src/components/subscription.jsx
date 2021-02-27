e397c65020b70ebfad356f1f7f27278531a4a0e8
prince <princeakpabio90@gmail.com>
2021-02-27 18:07:09 +0100

    Added scription component and changed app title in index.html

---
+++ client/src/components/subscription.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { CustomButton } from "./custom_button";
+
+const Subscription = () => (
+  <div className="subscription">
+    <span className="subscription_cta">
+      <p>Get the latest updates on new products and upcoming sales</p>
+    </span>
+    <div className="subscription_input">
+      <input className="subscription_input_email" value="Your Email Address" />
+      <CustomButton>Subscribe</CustomButton>
+    </div>
+  </div>
+);
+
+export default Subscription;
