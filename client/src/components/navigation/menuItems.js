6ab946af92986382e5f2cdb024c93857172670c7
prince <princeakpabio90@gmail.com>
2021-02-28 20:52:24 +0100

    Added navbar component with cart dropdown feature

---
+++ client/src/components/navigation/menuItems.js
@@ -0,0 +1,28 @@
+import logo from "../../assets/images/silksatinlogo-main.png";
+
+export const MenuItems = [
+  {
+    link: "/",
+    name: "Home",
+  },
+  {
+    link: "bio-component",
+    name: "About",
+  },
+  {
+    link: "project-component",
+    name: "Projects",
+  },
+  {
+    link: "client-component",
+    name: "Clients",
+  },
+  {
+    link: "contact-component",
+    name: "Contact",
+  },
+];
+
+export const LogoItem = {
+  logo: logo,
+};
