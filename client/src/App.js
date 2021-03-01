6b09c2a22a1c20b9b3cee0ea1a08d3798ffeff0a
prince <princeakpabio90@gmail.com>
2021-03-01 22:47:28 +0100

    added page information header for shop page

--- client/src/App.js
+++ client/src/App.js
@@ -3,11 +3,13 @@ import { Route, Switch } from "react-router-dom";
 import Footer from "./components/footer/footer";
 import Naviagtion from "./components/navigation/naviagtion";
 import HomePage from "./pages/home_page";
+import ShopPage from "./pages/shop_page";
 function App() {
   return (
     <div>
       <Naviagtion />
       <Switch>
+        <Route path="/shop" component={ShopPage} />
         <Route exact path="/" component={HomePage} />
       </Switch>
       <Footer />
